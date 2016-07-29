c89db1be38cde45b07d0ddea0598b9834a916a97
hisa <hisa@iporaitech.com>
2016-07-29 10:56:59 -0400

    Base configuration for Multi Page App with commons chunk

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+const pkg = require(path.resolve('../package.json'));
+
 module.exports = {
-  entry: "./web/static/js/app.js",
+  entry: {
+    app: "./web/static/js/app.js"
+  },
   output: {
     path: "./priv/static/js",
-    filename: "app.js"
-  }
+    filename: "[name]-bundle.js"
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('commons.chunk.js')
+  ],
+  loaders: [{
+    loader: 'babel',
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    query: {
+      plugins: ['transform-runtime'],
+      presets: ['es2015', 'stage-0', 'react']
+    }
+  }]
 };
