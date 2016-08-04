d31c58188de5ee7c86ff74539f5dc4e4b1a5ee9e
hisa <hisa@iporaitech.com>
2016-08-04 02:13:10 -0400

    Add Star Wars relay-graphql schema. Refs #12

--- web/graphql/schema.ex
+++ web/graphql/schema.ex
@@ -1,9 +1,45 @@
 defmodule Webapp.Web.GraphQL.Schema do
   use Absinthe.Schema
-  alias Webapp.Web.GraphQL.UserResolver, as: UserResolver
+  use Absinthe.Relay.Schema
+  alias Webapp.Web.GraphQL.UserResolver
+  alias Webapp.Web.GraphQL.StarWarsDB
+
   import_types Webapp.Web.GraphQL.Types
 
+  @desc """
+  This is the type that will be the root of our query,
+  and the entry point into our schema.
+
+  This implements the following type system shorthand:
+    type Query {
+      factions(names: [FactionName]): [Faction]
+      node(id: ID!): Node
+    }
+  """
   query do
+    field :rebels, :faction do
+      resolve fn
+        _, _ ->
+          StarWarsDB.get_rebels()
+      end
+    end
+
+    field :empire, :faction do
+      resolve fn
+        _, _ ->
+          StarWarsDB.get_empire()
+      end
+    end
+
+    node field do
+      resolve fn
+        %{type: node_type, id: id}, _ ->
+          StarWarsDB.get(node_type, id)
+        _, _ ->
+          {:ok, nil}
+      end
+    end
+
     @desc "Get all users"
     field :users, list_of(:user) do
       resolve &UserResolver.all/2
---
+++ web/graphql/star_wars_db.ex
@@ -0,0 +1,103 @@
+defmodule Webapp.Web.GraphQL.StarWarsDB do
+
+  @xwing %{
+    id: "1",
+    name: "X-Wing"
+  }
+
+  @ywing %{
+    id: "2",
+    name: "Y-Wing",
+  }
+
+  @awing %{
+    id: "3",
+    name: "A-Wing",
+  }
+
+  # Yeah, technically it's Corellian. But it flew in the service of the rebels,
+  # so for the purposes of this demo it"s a rebel ship.
+  @falcon %{
+    id: "4",
+    name: "Millenium Falcon"
+  }
+
+  @home_one %{
+    id: "5",
+    name: "Home One"
+  }
+
+  @tie_fighter %{
+    id: "6",
+    name: "TIE Fighter"
+  }
+
+  @tie_interceptor %{
+    id: "7",
+    name: "TIE Interceptor"
+  }
+
+  @executor %{
+    id: "8",
+    name: "Executor"
+}
+
+  @rebels %{
+    id: "1",
+    name: "Alliance to Restore the Republic",
+    ships: ["1", "2", "3", "4", "5"]
+  }
+
+  @empire %{
+    id: "2",
+    name: "Galactic Empire",
+    ships: ["6", "7", "8"]
+  }
+
+  @data %{
+    faction: %{
+      "1" => @rebels,
+      "2" => @empire
+    },
+    ship: %{
+      "1" => @xwing,
+      "2" => @ywing,
+      "3" => @awing,
+      "4" => @falcon,
+      "5" => @home_one,
+      "6" => @tie_fighter,
+      "7" => @tie_interceptor,
+      "8" => @executor
+    }
+  }
+
+  def data, do: @data
+
+  def get(node_type, id) do
+    case data |> get_in([node_type, id]) do
+      nil ->
+        {:error, "No #{node_type} with ID #{id}"}
+      result ->
+        {:ok, result}
+    end
+  end
+
+  def get_factions(names) do
+    factions = data.factions |> Map.values
+    names
+    |> Enum.map(fn
+      name ->
+        factions
+        |> Enum.find_value(&(&1 == name))
+    end)
+  end
+
+  def get_rebels do
+    {:ok, @rebels}
+  end
+
+  def get_empire do
+    {:ok, @empire}
+  end
+
+end
