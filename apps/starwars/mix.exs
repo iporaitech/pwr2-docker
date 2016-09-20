979fcd2a6ee8127236a82c2a75642206910e4c1b
Edipo <edipox@gmail.com>
2016-09-20 08:58:01 -0400

    Fix dependency paths in starwars app

--- apps/starwars/mix.exs
+++ apps/starwars/mix.exs
@@ -9,6 +9,10 @@ defmodule Starwars.Mixfile do
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",     
      aliases: aliases(),
      deps: deps()]
   end
