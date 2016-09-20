1d9bf33fa2194a1f6710127a70783c6d0e4d9823
Edipo <edipox@gmail.com>
2016-09-20 09:02:26 -0400

    Add starwars and greeter to webapp as dependencies in umbrella

--- apps/webapp/mix.exs
+++ apps/webapp/mix.exs
@@ -21,7 +21,7 @@ defmodule Webapp.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Webapp, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext, :starwars, :greeter]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,6 +37,9 @@ defmodule Webapp.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:starwars, in_umbrella: true},
+     {:greeter, in_umbrella: true}
+   ]
   end
 end
