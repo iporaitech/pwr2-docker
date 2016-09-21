6e540e584c6f063156cb96f92481ffee35328f9d
Edipo <edipox@gmail.com>
2016-09-21 16:26:13 -0400

    Remove phoenix deps from greeter app

--- apps/greeter/mix.exs
+++ apps/greeter/mix.exs
@@ -10,7 +10,7 @@ defmodule Greeter.Mixfile do
      lockfile: "../../mix.lock",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
+    #  compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -21,7 +21,7 @@ defmodule Greeter.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Greeter, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: []]
   end
 
   # Specifies which paths to compile per environment.
@@ -32,11 +32,6 @@ defmodule Greeter.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    []
   end
 end
