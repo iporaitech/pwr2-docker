085d392b6f598e249f4c63589201e3c3dee10475
hisa <hisa@iporaitech.com>
2016-09-26 11:57:38 -0400

    Refs #33 - Authentication and Authorization * Install and config Guardian, GuardianDB, Canada and Comonin
    Closes #34 - Setup backend testing tools
    * Install and config ESpec, ESpec.Phoenix, ExMachina and WhiteBread
    * Install and config mix test.watch to run specs automatically when saving a file.
    * Add spec dir and its files generated by `MIX_ENV=test mix espec.init`
    * Remove test dir because we're using spec instead

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :webapp, Webapp.Endpoint,
-  http: [port: 4001],
+  http: [port: {:system, "HTTP_PORT"}],
   server: false
 
 # Print only warnings and errors during test
@@ -12,8 +12,9 @@ config :logger, level: :warn
 # Configure your database
 config :webapp, Webapp.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_USER_PASSWORD"),
   database: "webapp_test",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST"),
+  port: 5432,
   pool: Ecto.Adapters.SQL.Sandbox
