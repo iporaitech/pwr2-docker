c95733322e983148c5137862ff7523b74f1feb5f
hisa <hisa@iporaitech.com>
2016-10-01 12:31:56 -0400

    Closes #43 - Add TEST_DB_NAME to docker-compose example and that variable in test.exs to config :database

--- apps/webapp/config/test.exs
+++ apps/webapp/config/test.exs
@@ -14,7 +14,7 @@ config :webapp, Webapp.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("DB_USER"),
   password: System.get_env("DB_USER_PASSWORD"),
-  database: "webapp_test",
+  database: System.get_env("TEST_DB_NAME"),
   hostname: System.get_env("DB_HOST"),
   port: 5432,
   pool: Ecto.Adapters.SQL.Sandbox
