5f65006ba64eeeb7f1641f617a8b34269d604cac
Javier Rios Selent <rsjavier14@gmail.com>
2016-11-04 12:15:29 -0300

    Reimplement Docs and Layout with Grid, Cell and others shared components. Refs #25

--- apps/webapp/web/static/js/shared/menu/Item.js
+++ apps/webapp/web/static/js/shared/menu/Item.js
@@ -1,11 +1,6 @@
-/**
- * file: shared/menu/MenuItem.js
- *
- * React components to render MDL menu components
- * See: https://github.com/google/material-design-lite/tree/v1.2.1/src/menu
- */
- import React, { PropTypes } from 'react';
- import classNames from 'classnames';
+// file: shared/menu/Item.js
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
 
 // MDL's js requires these styles to be imported globally
 import '!style!css!sass!material-design-lite/src/menu/_menu.scss';
