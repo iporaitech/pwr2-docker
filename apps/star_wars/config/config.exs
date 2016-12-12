ea1a41fd82867e0aabe63317e05b46002255ca36
hisa <hisa@iporaitech.com>
2016-12-12 15:21:19 -0300

    Add star_wars/graphql to Phoenix :reloadable_paths and move graphql out of star_wars web dir

--- apps/star_wars/config/config.exs
+++ apps/star_wars/config/config.exs
@@ -11,7 +11,8 @@ config :star_wars, ecto_repos: []
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:request_id],
+  reloadable_paths: ["lib", "graphql"]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
