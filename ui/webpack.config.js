83fa2b00597ee38f2be9ce892abeb2ef790069d2
hisa <hisa@iporaitech.com>
2016-12-24 01:09:35 -0300

    DRY babel-loader usage in webpack.config

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -2,14 +2,29 @@ const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+const buildBabelLoader = function(entryName) {
+  return {
+    test: /\.jsx?$/,
+    loader: 'babel',
+    include: path.resolve(`./${entryName}`),
+    query: {
+      plugins: [
+        path.resolve(`./${entryName}/src/babelRelayPlugin`),
+        'transform-runtime'
+      ],
+      presets: ['react', 'es2015', 'stage-0']
+    }
+  }
+}
+
 module.exports = {
   // devtool: "cheap-module-eval-source-map",
   devtool: "eval",
   context: __dirname,
   entry: {
     polyfill: "babel-polyfill",
-    star_wars: "./star_wars/src/index",
-    // core: "./core/index"
+    star_wars: "./star_wars/src",
+    core: "./core/src"
   },
   output: {
     path: "../apps/webapp/priv/static",
@@ -22,43 +37,23 @@ module.exports = {
     })
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel',
-      include: path.resolve('./star_wars'),
-      query: {
-        plugins: [
-          path.resolve('./star_wars/src/babelRelayPlugin'),
-          'transform-runtime'
-        ],
-        presets: ['react', 'es2015', 'stage-0']
+    loaders: [
+      buildBabelLoader('core'),
+      buildBabelLoader('star_wars'),
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'style?sourceMap',
+          [
+            'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+            'resolve-url',
+            'sass?sourceMap'
+          ]
+        )
+      },{
+        test: /\.json$/,
+        loader: 'json'
       }
-    },
-    // ,{
-    //   test: /\.jsx?$/,
-    //   loader: 'babel',
-    //   include: path.resolve('../star_wars/web/static/js'),
-    //   query: {
-    //     plugins: [
-    //       path.resolve('../star_wars/web/static/js/babelRelayPlugin'),
-    //       'transform-runtime'
-    //     ],
-    //     presets: ['react', 'es2015', 'stage-0']
-    //   }
-    // },
-    {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract(
-        'style?sourceMap',
-        [
-          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-          'resolve-url',
-          'sass?sourceMap'
-        ]
-      )
-    },{
-      test: /\.json$/,
-      loader: 'json'
-    }]
+    ]
   }
 };
