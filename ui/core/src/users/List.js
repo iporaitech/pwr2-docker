929b0f0cb5f995d63cfffd2b1ad5fa5c14ee3641
Javier Rios Selent <rsjavier14@gmail.com>
2017-01-03 16:56:28 -0300

    Add UserList * Add styles for users * Add queries for users

---
+++ ui/core/src/users/List.js
@@ -0,0 +1,80 @@
+// file: users/List.js
+
+import React, { PropTypes } from 'react';
+import Relay from 'react-relay';
+import { withRouter, Link } from 'react-router';
+
+// Base components
+import Grid, { Cell } from 'react-to-mdl/grid';
+import Textfield from 'react-to-mdl/textfield';
+import Select from 'react-select';
+import Button from 'react-to-mdl/button';
+import DataTable, {
+  DataTableHead,
+  DataTableTH,
+  DataTableTD
+} from 'react-to-mdl/dataTable';
+import Layout, { LayoutIcon } from 'react-to-mdl/layout';
+
+// CSS
+import CSSModules from 'react-css-modules';
+import { mdlUpgrade } from 'react-to-mdl';
+import styles from './styles.scss';
+
+class List extends React.Component {
+
+  static defaultProps = {
+    title: 'Lista de Usuarios'
+  }
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { title, users } = this.props;
+
+    return (
+      <div>
+        <Grid>
+          <Cell col={12}>
+            <h1 styleName="form-title">{title}</h1>
+          </Cell>
+        </Grid>
+        <Grid>
+          <Cell offset={3} col={6}>
+            <DataTable shadow={'2dp'}>
+              <DataTableHead>
+                <DataTableTH nonNumeric={true}> Usuario </DataTableTH>
+                <DataTableTH nonNumeric={true}> Email </DataTableTH>
+                <DataTableTH nonNumeric={true}> Acceso </DataTableTH>
+                <DataTableTH nonNumeric={true}> Insertado </DataTableTH>
+                <DataTableTH nonNumeric={true}></DataTableTH>
+              </DataTableHead>
+              <tbody>
+                {users.map((user, index) => {
+                  return <tr key={index}>
+                    <DataTableTD nonNumeric={true}>{`${user.firstName} ${user.lastName}`}</DataTableTD>
+                    <DataTableTD nonNumeric={true}>{user.email}</DataTableTD>
+                    <DataTableTD nonNumeric={true}>{user.role}</DataTableTD>
+                    <DataTableTD nonNumeric={true}>{user.role}</DataTableTD>
+                    <DataTableTD nonNumeric={true}>
+                      <Link to={`/admin/users/${user.id}/edit`}>editar </Link>
+                    </DataTableTD>
+                  </tr>
+                })}
+              </tbody>
+            </DataTable>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+/*** exports ***/
+export default withRouter(
+  mdlUpgrade(
+    CSSModules(List, styles)
+  )
+);
