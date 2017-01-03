d15eaa2042b26139276f0f85dd56c6aa1e748e54
Javier Rios Selent <rsjavier14@gmail.com>
2017-01-03 17:47:52 -0300

    Implement EditUserMutation both in backend as well as in frontend. Refs #59 * Add Edit Form

--- apps/core/graphql/users/types.ex
+++ apps/core/graphql/users/types.ex
@@ -58,6 +58,51 @@ defmodule Core.GraphQL.Users.Types do
           end
       end
     end
+
+    payload field :edit_user do
+      input do
+        field :id, non_null(:id)
+        field :first_name, non_null(:string)
+        field :last_name, non_null(:string)
+        field :phone, :string
+        field :email, non_null(:string)
+        field :role, non_null(:string)
+      end
+      output do
+        field :user, :user
+        field :errors, list_of(:string)
+      end
+      resolve fn
+        args, %{context: %{current_user: current_user}} ->
+          authorized = current_user |> can?(write User)
+
+          {:ok, data} = Absinthe.Relay.Node.from_global_id(args.id, Core.GraphQL.Schema)
+
+          edit = fn args ->
+            Repo.get!(User, data.id)
+              |> User.update_changeset(args)
+              |> Repo.update
+          end
+
+          with true <- authorized,
+            {:ok, %User{} = user} <- edit.(args)
+          do
+            {:ok, %{user: user}}
+          else
+            false ->
+              Core.GraphQL.Resolver.unauthorized_error
+            {:error, %{errors: errors}} ->
+              {:ok, %{
+                errors: Enum.map(errors, fn (err) ->
+                  {field, {field_err_msg, _type_info}} = err
+                  "#{field}: #{field_err_msg}"
+                end)
+              }}
+            {:error, message} ->
+              {:error, message}
+          end
+      end
+    end
   end # END object :user_mutations
 
 end
