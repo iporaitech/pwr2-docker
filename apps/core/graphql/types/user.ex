1f4e82da0c5dd799a07cdd7ae18baf3a60817a5e
Javier Rios Selent <rsjavier14@gmail.com>
2017-01-23 16:37:55 -0300

    Back to the old user mutation configuration.

--- apps/core/graphql/types/user.ex
+++ apps/core/graphql/types/user.ex
@@ -12,11 +12,10 @@ defmodule Core.GraphQL.Types.User do
   #
   object :user_mutations do
     @desc """
-    Add/Edit a User to DB if success
+    Add a User to DB if success
     """
-    payload field :user do
+    payload field :add_user do
       input do
-        field :id, non_null(:id)
         field :first_name, non_null(:string)
         field :last_name, non_null(:string)
         field :phone, :string
@@ -34,24 +33,66 @@ defmodule Core.GraphQL.Types.User do
 
           authorized = current_user |> can?(write User)
 
-          if Map.has_key?(args, :id) do
-            {:ok, data} = Absinthe.Relay.Node.from_global_id(args.id, Core.GraphQL.Schema)
-
-            insert_user = fn args ->
-              Repo.get!(User, data.id)
-                |> User.update_changeset(args)
-                |> Repo.update
-            end
-          else
-            insert_user = fn args ->
-              %User{}
-                |> User.registration_changeset(args)
-                |> Repo.insert
-            end
+          insert_user = fn args ->
+            %User{}
+              |> User.registration_changeset(args)
+              |> User.password_changeset(args)
+              |> Repo.insert
           end
 
           with true <- authorized,
             {:ok, %User{} = user} <- insert_user.(args)
+          do
+            {:ok, %{user: user}}
+          else
+            false ->
+              Core.GraphQL.Resolver.unauthorized_error
+            {:error, %{errors: errors}} ->
+              {:ok, %{
+                errors: Enum.map(errors, fn (err) ->
+                  {field, {field_err_msg, _type_info}} = err
+                  "#{field}: #{field_err_msg}"
+                end)
+              }}
+            {:error, message} ->
+              {:error, message}
+          end
+      end
+    end
+
+    @desc """
+    Edit a User to DB if success
+    """
+    payload field :edit_user do
+      input do
+        field :id, non_null(:id)
+        field :first_name, non_null(:string)
+        field :last_name, non_null(:string)
+        field :phone, :string
+        field :email, non_null(:string)
+        field :role, non_null(:string)
+        field :password, :string
+        field :password_confirmation, :string
+      end
+      output do
+        field :user, :user
+        field :errors, list_of(:string)
+      end
+      resolve fn
+        args, %{context: %{current_user: current_user}} ->
+
+          authorized = current_user |> can?(write User)
+
+          {:ok, data} = Absinthe.Relay.Node.from_global_id(args.id, Core.GraphQL.Schema)
+
+          update_user = fn args ->
+            Repo.get!(User, data.id)
+              |> User.registration_changeset(args)
+              |> Repo.update
+          end
+
+          with true <- authorized,
+            {:ok, %User{} = user} <- update_user.(args)
           do
             {:ok, %{user: user}}
           else
